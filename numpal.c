620a25a4e3d3ce8a50ba0328aa60546982b0ed48
Andre Luiz Abdalla Silveira <andreluizas@haskell.linux.ime.usp.br>
2014-05-09 17:08:27 -0300

    nao acabei

---
+++ numpal.c
@@ -0,0 +1,23 @@
+#inlude <stdio.h>
+int cont (int a){
+    int t;
+    while (a>0){
+        t++;
+        a=a/10;
+    }
+    return t;
+}
+
+int main (){
+    int a, t, ainv, b, n[100];
+    printf ("Dê o número A e B:\n");
+    sacnf ("%d %d", &a. &b);
+    t=cont(a);
+    for (i=0; i=t-1; i++){
+        n[i]=aux%10;
+        aux=aux/10;
+    }
+    for (i=t-1;i=0;i--){
+
+    }
+}
