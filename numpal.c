d8d2c15533d211b6fd32c85f1243b8e0fe81d309
Andre Luiz Abdalla Silveira <andreluizas@haskell.linux.ime.usp.br>
2014-05-09 17:29:39 -0300

    tem coisa errada?

--- numpal.c
+++ numpal.c
@@ -1,4 +1,13 @@
 #inlude <stdio.h>
+int pot(int base, int exp){
+     int result=1;
+     while (exp>0){
+        result=result*base;
+        exp--;
+     }
+     return result;
+}
+
 int cont (int a){
     int t;
     while (a>0){
@@ -9,7 +18,7 @@ int cont (int a){
 }
 
 int main (){
-    int a, t, ainv, b, n[100];
+    int a, t, ai=0, b, n[100];
     printf ("Dê o número A e B:\n");
     sacnf ("%d %d", &a. &b);
     t=cont(a);
@@ -17,7 +26,12 @@ int main (){
         n[i]=aux%10;
         aux=aux/10;
     }
-    for (i=t-1;i=0;i--){
-
+    for (i=t-1, p=0;i=0;i--,p++){
+        ai=ai+n[i]*pot(10,p);
     }
+    if (ai==b)
+        printf ("Os números %d e %d são palíndromos\n", a, b);
+    else
+        printf ("Os números %d e %d não são palíndromos\n", a, b);
+    retun 0;
 }
